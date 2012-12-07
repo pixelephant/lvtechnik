64c8859dbac13b3f3e4136ded7a84346bad5835f
Norbert Pap <norbert.pap@pixelephant.hu>
2012-12-07 16:23:20 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){               
+                
+        $('#slider-code').tinycarousel({
+        	display: 2
+        	//interval: true
+        });
+       
+        $(".fancybox").fancybox();
+
+        $("#length").change(function(){
+        	var val = $(this).val();
+        	var min = $(this).data("min");
+        	var max = $(this).data("max");
+        	var base = $(this).data("base");
+        	if(val < min){
+        		$(".dyn-price").text("Minimál").css("color","red");
+        	}
+        	else if(val > max){
+        		$(".dyn-price").text("Maximál").css("color","red");
+        	}
+        	else{
+        		$(".dyn-price").text(base*val+" Ft").css("color","#325A7C");	
+        	}
+        });
+
+    });
